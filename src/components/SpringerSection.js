8f13795622766cf8b5a2ac1a6b2e9601add1d806
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-04-28 21:41:00 +0200

    first commit

---
+++ src/components/SpringerSection.js
@@ -0,0 +1,24 @@
+export default function SpringerSection() {
+    return (
+      <section className=" bg-sky-50 h-auto ]md:h-[90vh] p-9 md:p-17 flex justify-center md:justify-start items-center w-full">
+        <div className="flex flex-col w-[90%] md:w-[50%] max-w-[900px]">
+          <h2 className="text-5xl md:text-5xl font-extrabold mb-4 text-sky-900 font-[Arial Black] tracking-tight pb-3">
+          Springer för glaciärerna
+          </h2>
+  
+          <h3 className="text-2xl md:text-1xl font-extrabold mb-4 font-[Arial Black] pb-5">
+          Med start i Abisko och målgång i Hemavan.
+          För varje krona vi samlar in springer vi en meter. Målet? 440 000 kronor.
+          </h3>
+  
+          <p className="pb-2">
+          Pengarna går till att finansiera dokumentären Guardians of the Glacier, en film om klimatförändringar och fjällvärldens framtid.
+          </p>
+          <p className="pb-2">
+          Men inte bara det – vi stödjer också Hands2Ocean i deras arbete för att rädda våra hav.
+          </p>
+        </div>
+      </section>
+    );
+  }
+  
---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import NavBar from "@/components/NavBar";
+import Header from "@/components/Header";
+import Intro from "@/components/Intro";
+import BannerKr from "@/components/BannerKr";
+import VideoSection from "@/components/VideoSection";
+import SpringerSection from "@/components/SpringerSection";
+import Documentary from "@/components/Documentary";
+
+export default function Home() {
+  return (
+    <>
+     <Head>
+        <title>Från Fjäll till Hav | Ett löpäventyr för vår planets framtid</title>
+        <meta
+          name="description"
+          content="Ett insamlingsäventyr där vi springer hela
+            Kungsleden, över 440 kilometer genom Sveriges vildaste natur, för att
+            sprida hopp och skapa förändring"
+        />
+      </Head>
+      <NavBar/>
+      <Header/>
+      <BannerKr/>
+      <Intro/>
+      <VideoSection/>
+      <SpringerSection/>
+      <Documentary/>
+
+      
+    </>
+  );
+}
