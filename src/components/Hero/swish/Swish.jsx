a0839ca1b1d40af35175b603602870efd2058573
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-06-03 17:47:30 +0200

    removed swish button

--- src/components/Hero/swish/Swish.jsx
+++ src/components/Hero/swish/Swish.jsx
@@ -6,7 +6,7 @@ export default function Swish() {
 
   return (
     <div className="flex flex-col justify-center items-center text-center text-white h-auto gap-7">
-      <SwishPaymentButton/>
+      {/* <SwishPaymentButton/> */}
       <QrCode />
       
     </div>
