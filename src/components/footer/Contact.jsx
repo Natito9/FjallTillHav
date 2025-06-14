954ae2ce3765cb3aa7adabf18dc3fb8a9226d295
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-06-14 21:55:48 +0200

    changed gmail to mailto

--- src/components/footer/Contact.jsx
+++ src/components/footer/Contact.jsx
@@ -3,7 +3,7 @@ export default function Contact() {
 <div>
         <h3 className="text-white text-lg">KONTAKT</h3>
      <a
-  href="https://mail.google.com/mail/?view=cm&to=contact@rookiis.com&su=Vill%20gärna%20komma%20i%20kontakt"
+  href="mailto:contact@rookiis.com?subject=Vill%20gärna&20komma&20i%20kontakt"
   target="_blank"
   rel="noopener noreferrer"
   className="hover:underline text-white"
