e773b263b0a48d51850b28006788f32aa411b680
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-07-03 19:11:35 +0200

    trying gtag again

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,20 +5,26 @@ export default function Document() {
     <Html lang="sv">
       <Head />
 
-
-        <title>Från Fjäll till Hav | Ett löpäventyr för vår planets framtid</title>
-  <meta
-    name="description"
-    content="Ett insamlingsäventyr där vi springer hela
+      <title>
+        Från Fjäll till Hav | Ett löpäventyr för vår planets framtid
+      </title>
+      <meta
+        name="description"
+        content="Ett insamlingsäventyr där vi springer hela
             Kungsleden, över 440 kilometer genom Sveriges vildaste natur, för att
             sprida hopp och skapa förändring"
-            url ="https://www.franfjalltillhav.se/"
-
-  />
+        url="https://www.franfjalltillhav.se/"
+      />
       <body className="antialiased">
-    
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQDXHZ87"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PQDXHZ87"
+            height="0"
+            width="0"
+           style={{ display: "none", visibility: "hidden" }}
+
+          ></iframe>
+        </noscript>
 
         <Main />
         <NextScript />
