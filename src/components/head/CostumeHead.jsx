e3135e5f233930b4db1201f9a02e0bc31b4bea73
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-07-03 19:58:29 +0200

    changed tag

--- src/components/head/CostumeHead.jsx
+++ src/components/head/CostumeHead.jsx
@@ -4,15 +4,17 @@ export default function CustomHead() {
   return (
     <Head>
    
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PQDXHZ87');`,
-        }}
-      />
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6S8SWDLR0R"></script>
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-6S8SWDLR0R');
+    `,
+  }}
+/>
 
       <title>Från Fjäll Till Hav</title>
       <meta
