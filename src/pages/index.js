3f2b9024f0bf15026f8b1e423dc7999a3807b024
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-07-08 13:24:50 +0200

    fixed map typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import Ocean from "@/components/Ocean";
 import Footer from "@/components/footer/Footer";
 import ScrollingLogos from "@/components/Hero/ScrollingLogos";
 import Sponsors from "@/components/Sponsors";
-import Map from "@/components/map";
+import Map from "@/components/Map";
 
 export default function Home() {
   return (
