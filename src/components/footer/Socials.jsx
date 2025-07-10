696f68512b9e697212d5d242567e60ec93e2179b
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-07-10 12:27:06 +0200

    insta footer redp

---
+++ src/components/footer/Socials.jsx
@@ -0,0 +1,15 @@
+export default function Socials() {
+  return (
+    <div>
+      <h3 className="text-white text-lg">SOCIALS</h3>
+      <a
+        href="https://www.instagram.com/rookiis_media"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:underline text-white"
+      >
+        Instagram
+      </a>
+    </div>
+  );
+}
