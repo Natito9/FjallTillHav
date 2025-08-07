d1562d74654f96ff0dbb04967f6d60c15bbd54fb
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-08-07 11:01:39 +0200

    new link to nav bar

--- src/components/FranFjallTillHav.jsx
+++ src/components/FranFjallTillHav.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function FranfjallTillHav() {
   return (
     <section
-      id="project"
+      id="presentation"
       data-theme="dark"
-      className="relative h-auto ]md:h-[90vh] px-4 py-15 md:p-17 flex justify-center md:justify-end items-center w-full"
+      className="relative h-auto ]md:h-[90vh] px-4 py-15 md:p-17 flex justify-center md:justify-end items-center w-full flex-col-reverse md:flex-row "
       style={{
         background: "rgba(0, 0, 0, 0.3)",
       }}
@@ -17,23 +17,25 @@ export default function FranfjallTillHav() {
         priority
         className="object-cover object-center -z-10"
       />
-      <div className="  text-white relative flex flex-col items-center w-[90%] md:w-[50%] max-w-[900px] ">
+
+      <div className="  text-white relative flex flex-col md:justify-start items-center w-[90%] md:w-[50%] max-w-[900px] ">
         <Image
           src="/assets/FFTHOttoProfile.jpg"
           alt="Otto Buckler"
-          width={200}
+          width={210}
           height={200}
-          className=" mb-4"
-        
+          className="mb-1 w-44 mt-4"
           priority
         />
-        <h3 className="pb-2 uppercase ">Otto Bücker - Föreläsare</h3>
-        <p className="text-center w-1/2">
-          “Vi sprang över 40mil - men det största vi
-hittade fanns inte på kartan.
-”   </p>
+
+        <h3 className=" text-s uppercase pb-4">Otto Bücker - Föreläsare</h3>
+        <p className="text-center w-1/2 ">
+          “Vi sprang över 40mil - men det största vi hittade fanns inte på
+          kartan. ”{" "}
+        </p>
       </div>
-      <div className=" text-white relative flex flex-col w-[90%] md:w-[50%] max-w-[900px] bg- backdrop-blur-xs ">
+   
+      <div className=" text-white relative flex flex-col w-[90%] md:w-1/2 max-w-[900px] md:text-left bg- backdrop-blur-xs ">
         <h1 className=" uppercase text-5xl md:text-5xl font-extrabold mb-4 font-[Arial Black] tracking-tight pb-3">
           Från Fjäll till Hav
         </h1>
@@ -69,7 +71,7 @@ hittade fanns inte på kartan.
           inte bara ett inspirationspass - utan ett konkret bidrag till
           förändring.)
         </p>
-        <p className="pb-2">
+        <p className="pb-2 text-lg">
           Boka via:{" "}
           <a
             href="mailto:contact@rookiis.com?subject=Vill%20gärna%20komma%20i%20kontakt"
@@ -79,6 +81,7 @@ hittade fanns inte på kartan.
           </a>
         </p>
       </div>
+         
     </section>
   );
 }
